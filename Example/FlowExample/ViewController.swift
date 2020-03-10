c66e99c378d8de9adbf6b714b8bd96d7238dd488
Nikolai Timonin <nikki.timonin@yandex.ru>
2020-03-10 23:31:41 +0300

    Initial commit

---
+++ Example/FlowExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowExample
+//
+//  Created by Nikolai Timonin on 10.03.2020.
+//  Copyright © 2020 HOME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Source/Flow.h
@@ -0,0 +1,19 @@
+//
+//  Flow.h
+//  Flow
+//
+//  Created by Nikolai Timonin on 10.03.2020.
+//  Copyright © 2020 HOME. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Flow.
+FOUNDATION_EXPORT double FlowVersionNumber;
+
+//! Project version string for Flow.
+FOUNDATION_EXPORT const unsigned char FlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flow/PublicHeader.h>
+
+
