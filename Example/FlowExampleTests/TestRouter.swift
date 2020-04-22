622abd7211eb16d027132964834bac2fe3873f66
Nikolai Timonin <nikki.timonin@yandex.ru>
2020-04-22 16:35:51 +0300

    Refactor navigate with action in test router

--- Example/FlowExampleTests/TestRouter.swift
+++ Example/FlowExampleTests/TestRouter.swift
@@ -10,41 +10,41 @@ import Flow
 
 class TestRouter: Router {
     
-    override func navigate(with action: BackAction, animated: Bool = true, completion: Completion? = nil) {
+    override func navigate(
+        with action: BackAction,
+        animated: Bool = true,
+        completion: Completion? = nil
+    ) {
                 
-                if let controllerToDismiss = action.dismiss {
-                    
-                    controllerToDismiss.dismiss(animated: animated) {
-                        
-                        if let controllerToPop = action.popTo {
-        
-                            controllerToPop.navigationController?.pop(
-                                to: controllerToPop,
-                                animated: false,
-                                completion: completion
-                            )
-        
-                        } else {
-        
-                            completion?()
-                        }
-                    }
-                    
-                    if action.popTo != nil { return }
-                }
+        if let controllerToDismiss = action.dismiss {
+            
+            controllerToDismiss.dismiss(animated: animated) {
                 
-                if let controllerToPop = action.popTo {
-
-                    let isAnimated = (action.dismiss == nil) ? animated : false
-
-                    controllerToPop.navigationController?.pop(
-                        to: controllerToPop,
-                        animated: isAnimated,
-                        completion: completion
-                    )
-                }
-        
+                self.pop(with: action, animated: false, completion: completion)
+            }
+            
+        } else {
+            
+            pop(with: action, animated: animated, completion: completion)
+        }    
     }
     
-    
+    private func pop(
+        with action: BackAction,
+        animated: Bool = true,
+        completion: Completion? = nil
+    ) {
+        guard let controllerToPop = action.popTo else {
+            
+            completion?()
+            
+            return
+        }
+
+        controllerToPop.navigationController?.pop(
+            to: controllerToPop,
+            animated: animated,
+            completion: completion
+        )
+    }
 }
